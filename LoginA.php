a1a268a24ab29f6d6033f6afe9756e08aecd87f4
Amina Ghafoor <aminaghafoor250@gmail.com>
2026-01-30 16:37:32 +0500

    extra h2 for testing

--- LoginA.php
+++ LoginA.php
@@ -62,6 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <div class="input-group">
     <h2>User Login:</h2>
+    <h2>User Login:</h2>
     <form action="LoginA.php" method="post">
 </div>
 
